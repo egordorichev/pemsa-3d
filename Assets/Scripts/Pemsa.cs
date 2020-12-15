e48306e5ee9ead17ca79016759e40bf19a10e033
Egor Dorichev <egordorichev@gmail.com>
2020-12-15 14:43:54 +0300

    Moving game texture into the display itself

--- Assets/Scripts/Pemsa.cs
+++ Assets/Scripts/Pemsa.cs
@@ -9,26 +9,6 @@ using AOT;
 
 [RequireComponent(typeof(AudioSource))]
 public class Pemsa : MonoBehaviour {
-	public static Color[] standardPalette = {
-			new Color(0, 0, 0),
-			new Color(29f / 255f, 43f / 255f, 83f / 255f),
-			new Color(126f / 255f, 37f / 255f, 83f / 255f),
-			new Color(0f / 255f, 135f / 255f, 81f / 255f),
-			new Color(171f / 255f, 82f / 255f, 54f / 255f),
-			new Color(95f / 255f, 87f / 255f, 79f / 255f),
-			new Color(194f / 255f, 195f / 255f, 199f / 255f),
-			new Color(255f / 255f, 241f / 255f, 232f / 255f),
-			new Color(255f / 255f, 0f / 255f, 77f / 255f),
-			new Color(255f / 255f, 163f / 255f, 0f / 255f),
-			new Color(255f / 255f, 236f / 255f, 39f / 255f),
-			new Color(0f / 255f, 228f / 255f, 54f / 255f),
-			new Color(41f / 255f, 173f / 255f, 255f / 255f),
-			new Color(131f / 255f, 118f / 255f, 156f / 255f),
-			new Color(255f / 255f, 119f / 255f, 168f / 255f),
-			new Color(255f / 255f, 204f / 255f, 170f / 255f)
-	};
-
-	public GameObject rawImage;
 	private static Color[] screenColorData = new Color[128 * 128];
 	static Texture2D screenTexture;
 	private static IntPtr emulator;
@@ -53,7 +33,7 @@ public class Pemsa : MonoBehaviour {
 		screenTexture = new Texture2D(128, 128, TextureFormat.RGBA32, false, true);
 		screenTexture.filterMode = FilterMode.Point;
 
-		rawImage.GetComponent<RawImage>().texture = screenTexture;
+		GetComponent<MeshRenderer>().materials[0].mainTexture = screenTexture;
 
 		AudioConfiguration audioConfig = AudioSettings.GetConfiguration();
 		audioConfig.sampleRate = PEMSA_SAMPLE_RATE / 2;
@@ -113,8 +93,8 @@ public class Pemsa : MonoBehaviour {
 		mouseMask |= Input.GetKey(KeyCode.Mouse1) ? 2 : 0;
 		mouseMask |= Input.GetKey(KeyCode.Mouse2) ? 3 : 0;
 
-		Vector2 point;
-		RectTransform rectTransform = rawImage.GetComponent<RectTransform>();
+		/*Vector2 point;
+		RectTransform rectTransform = GetComponent<RectTransform>();
 		RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, Camera.main, out point);
 
 		mouseX = point.x;
@@ -125,7 +105,7 @@ public class Pemsa : MonoBehaviour {
 		mouseY += rectTranslation.y;
 
 		mouseX = (mouseX / rectTransform.rect.width) * 128;
-		mouseY = (mouseY / rectTransform.rect.height) * 128;
+		mouseY = (mouseY / rectTransform.rect.height) * 128;*/
 
 		PemsaEmulator.UpdateEmulator(emulator, Time.deltaTime);
 
@@ -161,8 +141,8 @@ public class Pemsa : MonoBehaviour {
 			var rv = screenColor[val & 0x0f];
 			var lv = screenColor[val >> 4];
 
-			screenColorData[i * 2] = standardPalette[rv];
-			screenColorData[i * 2 + 1] = standardPalette[lv];
+			screenColorData[i * 2] = Palette.standard[rv];
+			screenColorData[i * 2 + 1] = Palette.standard[lv];
 		}
 	}
 
